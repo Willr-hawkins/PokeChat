2a64fe95424a8c9cc9277f95a735358ab3c38ab5
William Hawkins <Hawkinswill02@gmail.com>
2024-11-07 17:40:40 +0000

    Added delete post functionality, so users can delete posts they created

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.published_posts, name='home'),
     path('add_post/', views.add_post, name='add_post'),
+    path('delete/<int:post_id>/', views.delete_post, name='delete_post'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,15 +6,18 @@ from .models import Post
 from .forms import PostForm 
 
 def published_posts(request):
-    """ View to return a list of all posts with a status = 1. """
+    """
+    View to return a list of all posts with a status = 1.
+    """
     posts = Post.objects.filter(status=1).order_by('-created_on')
     
     return render(request, 'home/index.html', {'posts': posts})
 
 @login_required
 def add_post(request):
-    """ A view to return the PostForm so users can add thier posts. """
-
+    """
+    A view to return the PostForm so users can add thier posts.
+    """
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
@@ -31,3 +34,22 @@ def add_post(request):
     }
 
     return render(request, template, context)
+
+@login_required
+def delete_post(request, post_id):
+    """
+    Delete a post is the user is post.author.
+    """
+    post = get_object_or_404(Post, id=post_id, author=request.user)
+
+    if request.method == 'POST':
+        post.delete()
+        return redirect(reverse('profile'))
+
+    context = {
+        'post': post,
+    }
+
+    return render(request, 'home/delete_post.html', context)
+
+
