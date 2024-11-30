c19cdfb1149792dd3a1f5080d1469ff347411b4e
William Hawkins <Hawkinswill02@gmail.com>
2024-11-30 09:51:53 +0000

    Set DEBUG to true temporarily for heroku.

--- PokeChat/settings.py
+++ PokeChat/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://pokechat-100c878a0ff5.herokuapp.com', 'localhost']
 
