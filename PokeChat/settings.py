ef136ef63fff5e9336984802b21462016de1a2d6
William Hawkins <Hawkinswill02@gmail.com>
2024-11-30 09:59:00 +0000

    Fix allowed hosts for heroku to run

--- PokeChat/settings.py
+++ PokeChat/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://pokechat-100c878a0ff5.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['pokechat-59618d10cad5.herokuapp.com', 'localhost']
 
 
 # Application definition
